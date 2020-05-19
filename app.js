adf4b31ed377f15038227e30c842aad47c8fcb1f
bkalnay <baltakalnay@gmail.com>
2020-05-19 16:29:33 -0300

    edited

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
+
 const fs = require('fs');
 const PATH = './';
 const letter = 'a';
 
+
+let getFiles = (p)=>{
+    return fs.readdirSync(p);
+}
 
 let printFiles = (p) =>{
     fs.readdir(p, (err, files)=>{
@@ -20,11 +25,6 @@ let printFilesSorted = (p) =>{
 
 let count = (p, letra) =>{
     let c = 0;
-    /*getFiles(p).forEach(element => {
-        if(element.startsWith(letra)){
-            c++;
-        }
-    });*/
     fs.readdir(p, (err, files)=>{
         if(err){
             console.log(err)
